89544373019be8dd89dfac01edd5654e121a65ec
Graunephar <daniel@graungaard.com>
2019-03-02 00:54:44 +0100

    Added firefox and ch od to scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 
-sudo apt-get install nodejs npm git
+sudo apt-get install nodejs npm git xdotool firefox-esr
 
-git clone URL
+git clone https://github.com/Graunephar/PiSignage.git
 
 cd ~/PiSignage
+
+chmod 777 ./start.sh
 
 npm install
